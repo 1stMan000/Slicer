b05c21dffc724da2fafa2f515afb916d964e8c24
1stMan <1stmanleader@gmail.com>
2022-01-09 16:05:40 +0900

    Assiging bones system

---
+++ Assets/GetBones.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetBones : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        BoneWeight[] bones = GetComponentInChildren<SkinnedMeshRenderer>().sharedMesh.boneWeights;
+        Debug.Log(bones[0].boneIndex0);
+        Debug.Log(bones[1].boneIndex1);
+
+        Transform[] actualBones = GetComponentInChildren<SkinnedMeshRenderer>().bones;
+    }
+}
